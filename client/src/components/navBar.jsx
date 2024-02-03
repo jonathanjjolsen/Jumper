aa4283d6eb93bb17c805f8c9a0a17aa879b9291d
Jonathan Olsen <jonathanjolsen@gmail.com>
2024-02-03 14:38:15 -0700

    Fixed Nav Bar Title

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div className="navbar bg-base-100">
-            <a className="btn btn-ghost text-2xl">Olsen Adventures</a>
+            <a className="btn btn-ghost text-2xl">Jumper</a>
         </div>
     );
 }
