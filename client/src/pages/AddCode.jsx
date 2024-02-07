0b930fba28f82fe853bb01683ccada6e8cf70bcb
Jonathan <jonathanjolsen@gmail.com>
2024-02-06 21:23:34 -0700

    Visual Changes to Add Code Page

--- client/src/pages/AddCode.jsx
+++ client/src/pages/AddCode.jsx
@@ -3,19 +3,16 @@ import React from "react";
 
 const AddCode = () => {
     return (
-        <div className="centered">
-            <label className="form-control w-full max-w-xs">
-                <div className="label">
-                    <span className="label-text">What is your name?</span>
-                    <span className="label-text-alt">Top Right label</span>
-                </div>
-                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                <div className="label">
-                    <span className="label-text-alt">Bottom Left label</span>
-                    <span className="label-text-alt">Bottom Right label</span>
-                </div>
-            </label>
-        </div>
+        
+            <div className="centered">
+                <h1 className="text-3xl font-bold mb-8">Add Code</h1>
+                <label className="form-control w-full max-w-xs">
+                    <div className="label">
+                        <span className="label-text">What is your name?</span>
+                    </div>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </label>
+            </div>
     );
 }
 
