c5d1889009c79828863b49ea41a0f28de81ee572
Jonathan <jonathanjolsen@gmail.com>
2024-02-10 10:57:04 -0700

    test

--- client/src/pages/AddCode.jsx
+++ client/src/pages/AddCode.jsx
@@ -8,7 +8,7 @@ const AddCode = () => {
                 <h1 className="text-3xl font-bold mb-8">Add Code</h1>
                 <label className="form-control w-full max-w-xs">
                     <div className="label">
-                        <span className="label-text">What is your name?</span>
+                        <span className="label-text">Item</span>
                     </div>
                     <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                 </label>
